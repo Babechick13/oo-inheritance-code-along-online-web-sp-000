bc46f1acd816078e6190b3bb812d2912bb19fd4c
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-25 19:28:16 +0000

    Automatically backed up by Learn

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,21 +1,17 @@
 require "spec_helper"
-
 describe "car" do 
   let!(:car) { Car.new("medium", 4) } 
   describe "#new" do
-
     it "initializes with two arguments, wheel size and number" do 
       expect{Car.new("large", 4)}.to_not raise_error
     end
   end
-
   describe "#wheel_size" do 
     it "has a wheel_size" do
       car.wheel_size = "small"
       expect(car.wheel_size).to eq("small")
     end
   end
-
   describe "#wheel_number" do 
     it "has a wheel_number" do 
       car.wheel_number = 2      
@@ -25,13 +21,12 @@ describe "car" do
 
   describe "#go" do
     it "returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'" do 
+    it "returns 'vrrrrrrrooom!'" do 
       expect(car.go).to eq("VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
     end 
   end
 
-  describe "#fill_up_tank" do 
-    it "returns 'filling up!'" do 
       expect(car.fill_up_tank).to eq("filling up!")
     end
   end
-end
+
